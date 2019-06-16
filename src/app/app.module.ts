95f40045b870b3605937ab89062d5eb3126f16c8
ne8 <cpatrut@outlook.com>
2019-06-16 20:57:54 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridTestComponent } from './grid-test/grid-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridTestComponent
   ],
   imports: [
     BrowserModule,
