bfe6dbe078427771a89736de8be31c7616915558
ne8 <cpatrut@outlook.com>
2019-07-28 00:08:29 +0300

    sada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridTestComponent } from './grid-test/grid-test.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridTestComponent
+    GridTestComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
